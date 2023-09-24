36687cfe81ece6d1724bffd87ab6e70190a6bc6b
Antonio Ishac <antonioishac@gmail.com>
2023-09-24 19:33:29 -0300

    First commit

---
+++ src/test/java/br/com/isha/user/IshaUserApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.isha.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IshaUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
